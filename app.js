69de49863e6277cacea959a0931901410eecf08a
naomyleticia <naomyleticia92@gmail.com>
2021-06-25 14:27:51 -0300

    pocisionamento de rotas

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
-
-app.get("/", function(req, res) {
-    res.send("Bem vindo ao meu crud")
-})
-
+var bodyParser = require('body-parser');
+const handlebars = require("express-handlebars");
+const Pagamento = require("./models/Post")
 
 
+app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(bodyParser.json())
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/home', function(req, res) {
+    res.render('home');
+});
+
+app.get('/formulario', function(req, res) {
+    res.render('formulario.handlebars');
+});
+
+
+app.post('/form', function(req, res) {
+    //res.send("Nome: " + req.body.nome + "email: " + req.body.email + "mensagem: " + req.body.nome)
+    Pagamento.create({
+        name: req.body.name,
+        email: req.body.email,
+        mensagem: req.body.mensagem
+    }).then(function() {
+        res.send("Pagamento cadastrado")
+    }).catch(function(erro) {
+        res.send("erro é: " + erro)
+
+    })
+});
 
 
 
@@ -42,5 +68,5 @@ app.get("/", function(req, res) {
 
 
 app.listen(8081, function() {
-    console.log("servidor rodando")
+    console.log("esta rodando")
 })
